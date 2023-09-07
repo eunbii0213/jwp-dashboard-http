463ba87f530569b36b6b2b6e78adafba30fb1458
은비 <67318165+eunbii0213@users.noreply.github.com>
2023-09-08 08:47:11 +0900

    [톰캣 구현하기 1, 2단계] 주디(오은비) 미션 제출합니다. (#361)
    * feat: GET /index.html 응답 기능 구현
    
    * feat: CSS 지원 기능 구현
    
    * feat: Query String 파싱 기능 구현
    
    * feat: 로그인 성공시 index, 실패시 401로 리다이렉트하는 기능 구현
    
    * feat: 로그인 성공시 index, 실패시 401로 리다이렉트하는 기능 구현
    
    * feat: post 방식으로 회원가입 기능 구현
    
    * feat: Cookie에 JSESSIONID 값 저장 기능 구현
    
    * feat: Session 구현 기능 구현
    
    * refactor: 리팩터링
    
    * test: 테스트 코드 수정
    
    * fix: 회원가입시 index로 리다이렉트
    
    * feat: JsessionId cookie에 저장
    
    * feat: url enum 생성
    
    * fix: 회원가입시 자동 로그인 해제
    
    * refactor: url enum에 static 추가
    
    * refactor: header 전역 변수 설정
    
    * test: 테스트 작성
    
    * test: 테스트 작성
    
    * refactoring: 코드 리뷰 반영
    
    * test: 테스트 작성

--- tomcat/src/main/java/org/apache/coyote/http11/cookie/HttpCookie.java
+++ tomcat/src/main/java/org/apache/coyote/http11/cookie/HttpCookie.java
@@ -1,32 +1,24 @@
 package org.apache.coyote.http11.cookie;
 
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpCookie {
-    private final Map<String, String> cookies = new ConcurrentHashMap<>();
+    private String JSESSIONID = "";
 
     public HttpCookie() {
     }
 
-    // JSESSIONID 값 설정
-    public void addJSessionId(String sessionId) {
-        cookies.put("JSESSIONID", sessionId);
+    public String changeJSessionId(String jsessionId) {
+       JSESSIONID = jsessionId;
+       return JSESSIONID;
     }
 
-    // JSESSIONID 값 가져오기
+    public String createJSession() {
+        JSESSIONID = UUID.randomUUID().toString();
+        return JSESSIONID;
+    }
+
     public String getJSessionId() {
-        return cookies.get("JSESSIONID");
-    }
-
-    // 서버 응답 헤더에 Set-Cookie 추가
-    public String addCookieInResponseHeader() {
-        StringBuilder header = new StringBuilder();
-        header.append("HTTP/1.1 200 OK\r\n");
-        header.append("Content-Length: 5571\r\n");
-        header.append("Content-Type: text/html;charset=utf-8\r\n");
-        header.append("Set-Cookie: JSESSIONID=").append(getJSessionId()).append("\r\n");
-        return header.toString();
+        return JSESSIONID;
     }
 }
